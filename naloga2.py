10528edbae7020e0417428c176809ed29672b186
zanlukakralj <zan.kralj3@student.um.si>
2024-03-29 08:43:05 +0100

    Commited added file naloga2.py

---
+++ naloga2.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+
+def konvolucija(slika, jedro):
+    '''Izvede konvolucijo nad sliko z dodajanjem robov glede na velikost jedra.'''
+    pass
+
+def filtriraj_z_gaussovim_jedrom(slika, sigma):
+    '''Filtrira sliko z Gaussovim jedrom.'''
+    pass
+
+def filtriraj_sobel_horizontalno(slika):
+    '''Filtrira sliko z Sobelovim jedrom horizontalno in označi slikovne elemente z močnejšim gradientom.'''
+    pass
+
+if __name__ == '__main__':
+    pass
