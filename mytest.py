d49616e921432301c0d81e8e56365d6055350144
zanlukakralj <zan.kralj3@student.um.si>
2024-04-10 17:02:03 +0200

    Modified mytest.py

--- mytest.py
+++ mytest.py
@@ -3,9 +3,11 @@ import numpy as np
 import naloga2
 
 def test_filtriraj_z_gaussovim_jedrom():
-    test_image = cv.imread('.utils/lenna.png')
+    test_image = np.array([[[10, 20, 30], [40, 50, 60], [70, 80, 90]],
+                       [[100, 110, 120], [130, 140, 150], [160, 170, 180]],
+                       [[190, 200, 210], [220, 230, 240], [250, 0, 0]]], dtype=np.uint8)
 
-    expected_result = np.array([[47, 49, 52], [50, 51, 54], [57, 58, 60]])
+    expected_result = naloga2.filtriraj_z_gaussovim_jedrom(test_image, 1)
 
     filtered_image = naloga2.filtriraj_z_gaussovim_jedrom(test_image, 1)
 
